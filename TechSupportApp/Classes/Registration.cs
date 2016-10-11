056aee364257db972d619ba97125832dc4889a35
Christian <c.armstrong070>
2016-10-10 22:19:22 -0700

    Added the Customer, Product, and Registration classes into the folder

---
+++ TechSupportApp/Classes/Registration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport
+{
+   public class Registration
+    {
+        public int CustomerID { get; set; }
+
+        public string ProductCode { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+        
+    }
+}
