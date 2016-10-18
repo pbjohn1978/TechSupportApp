5a6cd548370f7ef6db841aaa9e941d8ffdbb82f1
BeekerMeMeMe(john) <pbjohn1978@yahoo.com>
2016-10-18 01:14:51 -0700

    Added AddCustomerToDB(Customer cust) to HelperDB class Added IsCustInDB(Customer cust) to HelperDB Both are not functioning yet... don't use them yet... :)

---
+++ TechSupportApp/Classes/CustomerValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupportApp.Classes
+{
+    public class CustomerValidator
+    {
+        public static bool IsValidCustomer(object sender, EventArgs e)
+        {
+            bool isValidInfo = true;
+
+            //TODO: validation code here... :)
+
+            return isValidInfo;
+        }
+    }
+}
--- TechSupportApp/Classes/HelperDB.cs
+++ TechSupportApp/Classes/HelperDB.cs
@@ -10,6 +10,8 @@ namespace TechSupportApp.Classes
 {
     public static class HelperDB
     {
+
+
         /// <summary>
         /// Code By: BeekerMeMeMe
         /// this methoud takes in no arguments and returns a List<Customer> ... :)
@@ -75,6 +77,7 @@ FROM[TechSupport].[dbo].[Customers]";
 
             return customers;
         }
+        
         /// <summary>
         /// Code By: BeekerMeMe
         /// this code takes in no arguments and returns the connection string to Database server...
@@ -88,5 +91,134 @@ FROM[TechSupport].[dbo].[Customers]";
         {
             return @"Data Source=DESKTOP-GNOQRKG\SQLEXPRESS;Initial Catalog=TechSupport;Integrated Security=True";
         }
+
+        /// <summary>
+        /// this methoud will take in a Customer Object and add it to the database... Please ensure that the Customer object is a valid Customer object 
+        ///     AND THAT THE CUSTOMER ID NUMBER IS NOT TAKEN BEFORE CALLING THIS METHOUD... :)
+        /// this methoud will work for updating a current customer or adding a new customer... just give it a customer object it'll take care of it for ya... :)
+        /// </summary>
+        /// <param name="cust"></param>
+        /// <returns> bool : true = Customer added, False = Customer was NOT added</returns>
+        public static bool AddCustomerToDB(Customer cust)
+        {
+            throw new NotImplementedException();
+            bool isInDB = false;
+            //the following if statement will update the customers info if true
+            //if false it will add the New Customer to the database... :)
+            if (HelperDB.IsCustInDB(cust))
+            {   
+                
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = @"UPDATE[dbo].[Customers] SET [Name] = @name , [Address] = @addre, [City] = @city , [State] = @state , [ZipCode] = @zipco , [Phone] = @phone , [Email] = @email
+WHERE CustomerID = @id ";
+                cmd.Parameters.AddWithValue("@id", cust.CustomerID.ToString());
+                cmd.Parameters.AddWithValue("@addre", cust.Address);
+                cmd.Parameters.AddWithValue("@city", cust.City);
+                cmd.Parameters.AddWithValue("@email", cust.Email);
+                cmd.Parameters.AddWithValue("@name", cust.Name);
+                cmd.Parameters.AddWithValue("@phone", cust.Phone);
+                cmd.Parameters.AddWithValue("@state", cust.State);
+                cmd.Parameters.AddWithValue("@zipco", cust.ZipCode);
+                try
+                {
+                    con.Open();
+                    int rows = cmd.ExecuteNonQuery();
+                    if (rows == 1)
+                        isInDB = true;
+                }
+                catch (SqlException ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    con.Close();
+                }
+                return isInDB;
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection(GetConnectionString());
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = @"INSERT INTO [dbo].[Customers]([Name],[Address],[City],[State],[ZipCode],[Phone],[Email])
+VALUES(@name,@addre,@city,@state,@zipco,@phone,@email)";
+
+                cmd.Parameters.AddWithValue("@addre", cust.Address);
+                cmd.Parameters.AddWithValue("@city", cust.City);
+                cmd.Parameters.AddWithValue("@email", cust.Email);
+                cmd.Parameters.AddWithValue("@name", cust.Name);
+                cmd.Parameters.AddWithValue("phone", cust.Phone);
+                cmd.Parameters.AddWithValue("@state", cust.State);
+                cmd.Parameters.AddWithValue("zipco", cust.ZipCode);
+
+                try
+                {
+                    con.Open();
+                    int rows = cmd.ExecuteNonQuery();
+                    if (rows == 1)
+                        isInDB = true;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    con.Close();
+                }
+                return isInDB;
+            }
+        }
+
+        /// <summary>
+        /// this methoud will take in a Customer object an will return a bool representing weather or not the Customer is already in the db... :)
+        ///     Please ensure the Customer is a valid Customer object before calling this methoud... :)
+        /// </summary>
+        /// <param name="cust">Customer Object</param>
+        /// <returns>bool representing weather or not the customer is in the database</returns>
+        public static bool IsCustInDB(Customer cust)
+        {
+            throw new NotImplementedException();
+            bool isCustInTheDB;
+
+            SqlConnection con = new SqlConnection(GetConnectionString());
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = @"SELECT[CustomerID],[Name],[Address],[City],[State],[ZipCode],[Phone],[Email]
+FROM[TechSupport].[dbo].[Customers]
+where CustomerID=@custid";
+
+            cmd.Parameters.AddWithValue("@custid", cust.CustomerID);
+
+            try
+            {
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    isCustInTheDB = true;
+                }
+                else
+                {
+                    isCustInTheDB = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return isCustInTheDB;
+        }
+
+
+
     }
 }
