f75a32a6d388d7fbf781813f921fbcd7102d8402
makdevio <richards7628@student.cptc.edu>
2016-10-20 11:47:14 -0700

    Adding Incidents.cs and Technicians.cs

---
+++ TechSupportApp/Classes/Incidents.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupportApp.Classes
+{
+    public class Incidents
+    {
+        public int IncidentID { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public string ProductCode { get; set; }
+
+        public DateTime DateOpened { get; set; }
+
+        public DateTime? DateClosed { get; set; }
+
+        public String Title { get; set; }
+
+        public String Description { get; set; }
+    }
+}
