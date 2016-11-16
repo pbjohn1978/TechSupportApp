e80d3db69703a10ba6adb76f4d1c9685510ef7eb
makdevio <richards7628@student.cptc.edu>
2016-11-15 20:50:02 -0800

    completed validation for zip code, email, and phone number.

--- TechSupportApp/Classes/Validation.cs
+++ TechSupportApp/Classes/Validation.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TechSupport;
 
@@ -13,9 +15,45 @@ namespace TechSupportApp.Classes
         {
             bool isValidInfo = true;
 
-            //TODO: validation code here... :)
+           
 
             return isValidInfo;
         }
+        public static bool IsValidZipCode(string zipCode)
+        {
+            string regexPattern = @"^(\d{5})(-\d{4})?$";
+            Match validZipCode = Regex.Match(zipCode, regexPattern);
+            if (!validZipCode.Success)
+            {
+                MessageBox.Show("Invalid email address. Please enter valid zip code");
+                return false;
+            }
+            return true;
+        }
+        public static bool IsValidEmail(string email)
+        {
+            string regexPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
+            Match validEmail = Regex.Match(email, regexPattern);
+            if (!validEmail.Success)
+            {
+                MessageBox.Show("Invalid email address");
+                return false;
+            }
+            return true;
+        }
+
+        public static bool IsValidPhoneNumber(string phoneNumber)
+        {
+            string regexPattern = "^(?([0-9]{3}))?[-]?([0-9]{3})[-]?([0-9]{4})$";
+            Match validPhoneNumber = Regex.Match(phoneNumber, regexPattern);
+            if (!validPhoneNumber.Success)
+            {
+                MessageBox.Show("Invalid phone number.  Please enter in valid format:  xxx-xxx-xxxx");
+                return false;
+            }
+            return true;
+        }
+
     }
+
 }
